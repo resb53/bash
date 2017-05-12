dda540bd00dcc1fbd0ee727aa2c962e34956ca84
resb53 <resb53.git@gmail.com>
2017-05-12 17:48:19 +0000

    Initial commit.

---
+++ .bashrc
@@ -0,0 +1,41 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# Set user and mask
+USER=$(whoami)
+umask 0002
+
+# Git prompt details
+source ~/.git_ps1_detail.bash
+
+# Set prompt to dynamic update
+export PROMPT_COMMAND="echo -ne \"\033]0;${USER}@${HOSTNAME} -- $(ddate)\007\""
+export PS1='\033[00;96m$(date +%s) \033[00;36m[\u@\h] \033[00;96m${PWD}$(__git_ps1_detail)\n$ '
+
+# User specific exports
+export KERN_DIR=/usr/src/kernels/`uname -r`
+export SHELL=/bin/bash
+export EDITOR=vim
+export HISTFILESIZE=10000
+export CHARSET="utf8"
+export LANG="en_GB.UTF-8"
+export TZ="UTC"
+
+# Set Go vars
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+
+# User specific aliases
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+alias ls="ls --color=auto"
+alias ll="ls -lahF"
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
+alias hex="/usr/bin/od -A x -t x1z -v"
---
+++ .git_ps1_detail.bash
@@ -0,0 +1,33 @@
+source /usr/share/git-core/contrib/completion/git-prompt.sh
+
+function __git_ps1_detail() {
+
+  BRANCH_STRING=''
+  STATUS=$(git status -s 2> /dev/null)
+  
+  if [ "$?" -eq 0 ]; then
+    # Any untracked changes to files
+    UNTRK=$(echo $STATUS | grep -e "^??" 2> /dev/null | wc -l)
+    # Any changes to tracked files
+    CHNGE=$(echo "$STATUS" | grep -e "^[MADRCU? ][MD]" 2> /dev/null | wc -l)
+    # Any uncommitted changes
+    UNCOM=$(echo "$STATUS" | grep -e "^[MADRCU?] " 2> /dev/null | wc -l)
+    # Total tracked files
+    TRACK=$(git ls-files | wc -l)
+    
+    # Provides the branch name and 4 colon separated numbers.
+    # Changes propagate from left to right as follows:
+    # Red:    New files that aren't tracked.
+    # Orange: Tracked files that have uncommitted changes.
+    # Yellow: Files staged for committing.
+    # Green:  Total tracked files in the repo.
+    BRANCH_STRING+=$'\033[0;32m'$(__git_ps1 " [%s:")$'\033[0;31m'${UNTRK}$'\033[0;32m':$'\033[0;33m'${CHNGE}$'\033[0;32m':$'\033[0;93m'${UNCOM}$'\033[0;32m':$'\033[0;92m'${TRACK}$'\033[0;32m]'$'\033[0m'
+
+  else
+    # Not a git repo or git not installed
+    BRANCH_STRING+=$'\033[0m'
+  fi
+
+  echo ${BRANCH_STRING}
+
+}
